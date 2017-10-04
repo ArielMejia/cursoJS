fff97fccb266276fa23f27d7a7fe77ae25130d41
ArielMejia <kevinmejia_2010@hotmail.com>
2017-10-04 00:46:03 -0600

    Variables y tipos de datos

---
+++ js/variables.js
@@ -0,0 +1,43 @@
+var nombre = "Kevin"; //variable
+//nombre = prompt("Ingresa tu nombre"); //esta funcion permite almacenar un nombre y un valor
+//edad = prompt("ingresa tu edad");
+saldo = 1000.55; //valor float
+//alert(nombre);
+//document.write(nombre + " " + edad);
+//console.log(nombre);
+
+//----Tipos de datos----
+
+	// Numerico - float
+	var costo = 50.5;
+
+	// String - Cadena de texto
+	var nombre = "Kevin";
+	var apellidos = "Mejia Deras";
+	var texto = "Programar es ser \"feliz\""
+	
+	// Array - Arreglo
+	var paises = ["Honduras", "Guatemala", "Nicaragua"];
+	//document.write(paises[1]);
+
+	//Object - Objeto
+	var objeto = {
+		color:"Black",
+		size: 12
+	}
+
+	//Boolean -boleano
+	var boleano = true;
+	var boleano = false;
+
+	// Undefined
+	var precio;
+	//document.write(precio);
+
+	//Null
+	var numero = 10;
+	var numero = null;
+
+	//NaN  es un valor ilogico, ejemplo multiplicar un numero por texto
+	
+
